bd021fe2e19709f16e545aa69da7598f642e983f
ev <adam.prycki@gmail.com>
2019-07-11 04:27:53 +0200

    Update, working send in case of missing subvols and complete send mode

--- bss.sh
+++ bss.sh
@@ -3,9 +3,10 @@
 
 IFS_bk=$IFS
 
+temp_send_on_err_var=1
 
 echo_msg(){
-	if [ DEBUG == "-1" ] ;then 
+	if [[ $DEBUG == "-1" ]] ;then 
 		echo $@
 	fi
 	case $1 in
@@ -13,7 +14,7 @@ echo_msg(){
 			echo $@
 		;;
 		"~~")
-			if [ DEBUG == "1" ] ;then
+			if [[ $DEBUG == "1" ]] ;then
 				echo $@
 			fi
 		;;
@@ -52,21 +53,24 @@ delsubvol () {
 	echo_msg $btrfs sub $subnd
 }
 
-#doesexist_ossh (){	#unused
-#	detemp=$(ssh -i $sshid $sshuh "btrfs sub show $snapsendloc/$subn} ")
-#	if [[ -e $detemp  ]] ; then
-#		echo_msg == snapshot exists
-#		if [[ -e $(detemp|grep readonly) ]] ; then
-#			echo_msg == snapshot read only, probably fine
-#			return 0
-#		else
-#			echo_msg !! snapshot incomplete
-#	#		delsubvol()
-#		fi
-#	echo_msg !! "snapshot trasnfer failed"
-#	#subvoldel()
-#	fi
-#}
+doesexist (){
+	ssh -i $sshid $sshuh "btrfs sub show $snapsendloc/$subn}"
+	ec=$?
+	#if [[ -e $detemp  ]] ; then
+	if test -s $ec
+	then
+		echo_msg == snapshot exists
+		if [[ -e $(detemp|grep readonly) ]] ; then
+			echo_msg == snapshot read only, probably fine
+			return 0
+		else
+			echo_msg !! snapshot incomplete
+	#		delsubvol()
+		fi
+	echo_msg !! "snapshot trasnfer failed"
+	#subvoldel()
+	fi
+}
 
 #transp () { #unused
 #	case $transp in
@@ -98,18 +102,18 @@ send_with () {
 				inc)
 					echo_msg ~~ "btrfs send -p $snapdir/$snapp $snapdir/$snapf |pv| ssh -i $sshid $sshuh btrfs receive $snapsendloc"
 					btrfs send -p $snapdir/$snapp $snapdir/$snapf |pv| ssh -i $sshid $sshuh btrfs receive $snapsendloc
-					ec=${PIPESTATUS[3]}
-					break
+#echo_msg ~~ DEBUG EC 1-${PIPESTATUS[1]} 2-${PIPESTATUS[2]} 3-${PIPESTATUS[3]} 4-${PIPESTATUS[4]}
+					ec=${PIPESTATUS[2]}
+					return $ec
 					;;
 				comp)
 					echo_msg ~~ "btrfs send $snapdir/$snapf |pv| ssh -i $sshid $sshuh btrfs receive $snapsendloc"
 					btrfs send $snapdir/$snapf |pv| ssh -i $sshid $sshuh btrfs receive $snapsendloc
-					ec=${PIPESTATUS[3]}
-					break
+					ec=${PIPESTATUS[2]}
+					return $ec
 					;;
 				*)
 					echo_msg !! "error"
-					break
 					;;
 			esac
 			#case $ec in
@@ -132,7 +136,7 @@ send_with () {
 					echo_msg ~~ "btrfs send -p $snapdir/$snapp $snapdir/$snapf |pv| nc $ip 9999 -q 0"
 					btrfs send -p $snapdir/$snapp $snapdir/$snapf |pv| nc $ip 9999 -q 0
 					ec=${PIPESTATUS[1]}
-					break
+					return $ec
 					;;
 				comp)
 					echo_msg ~~ "ssh -i $sshid -f $sshuh 'nc -l -p 9999 -w 5 |btrfs receive '$snapsendloc"
@@ -141,7 +145,7 @@ send_with () {
 					echo_msg ~~ "btrfs send $snapdir/$snapf |pv| nc $ip 9999 -q 0"
 					btrfs send $snapdir/$snapf |pv| nc $ip 9999 -q 0
 					ec=${PIPESTATUS[1]}
-					break
+					return $ec
 					;;
 				*)
 					echo_msg !! "error"
@@ -157,25 +161,25 @@ send_with () {
 					echo_msg ~~ "btrfs send -p $snapdir/$snapp  $snapdir/$snapf |pv| btrfs receive $snapsendloc"
 					btrfs send -p $snapdir/$snapp  $snapdir/$snapf |pv| btrfs receive $snapsendloc
 					ec=${PIPESTATUS[3]}
-					break
+					return $ec
 					;;
 				comp)
 					echo_msg !! "sending complete subvolume"
 					echo_msg ~~ "btrfs send $snapdir/$snapf |pv| btrfs receive $snapsendloc"
 					btrfs send $snapdir/$snapf |pv| btrfs receive $snapsendloc
 					ec=${PIPESTATUS[3]}
-					break
+					return $ec
 					;;
 				*)
 					echo_msg !! "error"
+					return 1
 					;;
 			esac
-
 			return $ec
-		;;
+			;;
 		*)
-		echo_msg !! "network option unhandled"
-		return 2
+			echo_msg !! "network option unhandled"
+			return 2
 		;;
 	esac
 }
@@ -185,7 +189,14 @@ mount /mnt/a/		## this will be fixed, filesystem needs to be mouned on my partic
 IFS='
 '
 
-for x in $(grep -v "#" /etc/bss.conf) ; do
+if test $1==""	#test for empty $1 argument
+then
+	conf_f="/etc/bss.conf"
+else
+	conf_f=$1
+fi
+
+for x in $(grep -v "#" $conf_f) ; do
 	IFS=' 	' read -r -a linearray <<< $x
 	subn=${linearray[0]} 
 	snapfs=${linearray[1]}
@@ -195,10 +206,12 @@ for x in $(grep -v "#" /etc/bss.conf) ; do
 	sshid=${linearray[5]}
 	subkeep=${linearray[6]}
 	transp=${linearray[7]}
+#	cmprs=${linearray[8]}
+#	cmprs_de=${linearray[8]}
 	ip=$(echo $sshuh | cut -d '@' -f 2)
 	subnd="$subn-r-$(date +'%y%m%d')"
 
-	if [ "DEBUG" == '1' ] ; then 
+	if [[ $DEBUG == '1' ]] ; then 
 		echo_msg ~~ "------------------------------------"
 		echo_msg ~~ "subn		"$subn
 		echo_msg ~~ "snapfs		"$snapfs
@@ -207,12 +220,21 @@ for x in $(grep -v "#" /etc/bss.conf) ; do
 		echo_msg ~~ "sshuh		"$sshuh
 		echo_msg ~~ "sshid		"$sshid
 		echo_msg ~~ "subkeep		"$subkeep
-		echo_msg ~~ "trans		"$trans
+		echo_msg ~~ "transp		"$transp
 		echo_msg ~~ "'ip(from sshuh)'	"$ip
 		echo_msg ~~ "subnd		"$subnd
 		echo_msg ~~ "------------------------------------"
 	fi
 	
+#	if ! [[ $cmprs == "" ]]		#unfinished
+#	then
+#		if test $cmprs == "lzma" || test $cmprs == "xz" || test $cmprs == "pxz"
+#		then
+#			echo_msg !!
+#	fi
+	
+#	cmprs="${linearray[8]} -z "
+#	cmprs_de="${linearray[8]} -d "
 	
 	#if [[ ! -d $snapdir/$subd ]]; then #doesn't work and I don't know why :/
 	if [[ -z $( ls -1 $snapdir | grep $subnd ) ]]; then
@@ -225,12 +247,13 @@ for x in $(grep -v "#" /etc/bss.conf) ; do
 		echo_msg 	~~ "snapf	"$snapf
 
 		send_with $transp inc
-
-		if (( $? == 1 )) ; then
+		ec=$?
+		echo_msg ~~ sned exit code $ec
+		if (( $ec == 1 )) ; then
 			echo_msg !! "error while sending subvolume"
 			for x in $(seq 3 $subkeep) ; do
 				snapp=$(ls $snapdir -1|grep $subn|sort -r|sed -n $x'p')
-				if [ $snapp == "" ] ;then
+				if [[ $snapp == "" ]] ;then
 					echo !! "subvolume missing"
 					ec=1
 					break
@@ -246,16 +269,12 @@ for x in $(grep -v "#" /etc/bss.conf) ; do
 				echo_msg !! "all avalible parent subvolumes failed. Attempting to send complete snapshot"
 				send_with $transp comp 
 			fi
-
-
 		fi
 	else
 		echo_msg !! "snapshot already exists!"
 	fi
 	
-	
 	for x in $(ls -1 $snapdir |grep $subn|sort -r|sed -n $subkeep',$p'); do 
-	
 		echo_msg ~~ "btrfs sub del "$snapdir'/'$x
 		btrfs sub del $snapdir/$x
 	done
@@ -273,8 +292,7 @@ for x in $(grep -v "#" /etc/bss.conf) ; do
 	unset trans
 	unset ip
 	unset subnd
-
-
+	unset ec
 done
 
 IFS=$IFS_bk
